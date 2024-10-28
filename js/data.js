794af76a184de491a08fc4bf1dbf1a9ab4516d71
o-oshibka <166124127+o-oshibka@users.noreply.github.com>
2024-10-28 16:26:20 +0500

    5.14

--- js/data.js
+++ js/data.js
@@ -1,40 +1,52 @@
-import { getRandomInt, getRandomElement } from './util.js';
+import { getRandomInteger, getRandomArrayElement } from './util.js';
 
-const commentsMessages = [
+const messages = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
 const names = [
   'Артём',
   'Мария',
-  'Дмитрий',
-  'Екатерина',
-  'Сергей',
-  'Анна',
   'Иван',
   'Ольга',
-  'Александр',
-  'Татьяна'
+  'Сергей',
+  'Елена',
+  'Дмитрий',
 ];
 
 export function generateComments() {
-  const commentsCount = getRandomInt(0, 30);
+  const commentsCount = getRandomInteger(0, 30); // Случайное количество комментариев
   const comments = [];
 
   for (let i = 0; i < commentsCount; i++) {
     const comment = {
       id: i + 1,
-      avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
-      message: getRandomElement(commentsMessages),
-      name: getRandomElement(names)
+      avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+      message: getRandomArrayElement(messages),
+      name: getRandomArrayElement(names)
     };
     comments.push(comment);
   }
 
   return comments;
+}
+
+export function createPhoto(id) {
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: `Описание фотографии номер ${id}`,
+    likes: getRandomInteger(15, 200),
+    comments: generateComments() // коммент
+  };
+}
+
+export function generatePhotos() {
+  const photos = [];
+
+  for (let i = 1; i <= 25; i++) {
+    photos.push(createPhoto(i)); // фото в массив
+  }
+  return photos;
 }
